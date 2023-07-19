35afb962984d9a1dcf23ff86fdba598e47538bdd
khushiRajpoot02 <khushisinghbhu02@gmail.com>
2023-07-19 22:04:04 +0530

    "Episode-3"

--- App.js
+++ App.js
@@ -1,36 +1,33 @@
-/**
- *
- * <div id="parent">
- *      <div id="child">
- *         <h1>I'm h1 tag</h1>
- *         <h2>I'm h1 tag</h2>
- *      </div>
- * <div id="child2">
- *         <h1>I'm h1 tag</h1>
- *         <h2>I'm h1 tag</h2>
- *      </div>
- * </div>
- *
- * ReactElement(Object) => HTML(Browser Understands)
- */
-// checked for committing changes
 import React from "react";
 import  ReactDOM  from "react-dom/client";
-const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child" }, [
-      React.createElement("h1", {}, "I'm an h1 tag"),
-      React.createElement("h2", {}, "I'm an h2 tag"),
-    ]),
-    React.createElement("div", { id: "child2" }, [
-      React.createElement("h1", {}, "I'm an h1 tag"),
-      React.createElement("h2", {}, "I'm an h2 tag"),
-    ]),
-  ]);
-  
-  //JSX
-  
-  console.log(parent); // object
-  
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  
-  root.render(parent);
+//learning react elemnts and components
+// let's convert elemnts to components
+const JsxHeading1 = ()=>{
+    return <h1>Converting React elemnts to components</h1>;
+}
+const jsxHeading = <h1 className="heading">Hey this is jsx!</h1>
+// creating components
+const Title = ()=>{
+    return(
+        <div className="main">
+        <h3>Here is the title components</h3>
+        <h3>Let's add this into HeadingComponents</h3>
+        <JsxHeading1/>
+        </div>
+    )
+}
+const CheckingBrackets = ()=>(
+    <h1>Hey this is smallBracket used code!</h1>
+)
+const HeadingComponents = ()=>{
+    return(
+        <div id = "container">
+        <h2>This is heading components</h2>
+         <Title/>
+         <CheckingBrackets/>
+        </div>
+    ) 
+} 
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponents/>);
